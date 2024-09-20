2ecdacf3d330185aac01e5471dcd576689dc9344
Roger Urich <roger.urich@shiftpixy.com>
2024-09-20 02:39:16 -0700

    redis as an env

--- app/lib/redis.ts
+++ app/lib/redis.ts
@@ -2,7 +2,7 @@ import { createClient } from "redis";
 
 async function initializeRedisClient() {
   const client = createClient({
-    url: "redis://localhost:6379",
+    url: process.env.REDIS_URL,
   });
 
   client.on("error", (err) => console.log("Redis Client Error", err));
